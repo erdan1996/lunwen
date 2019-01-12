300eab667e3a49dc086c6a5bb0b897f4c13b725b
erdan1996 <604691708@qq.com>
2019-01-12 18:07:03 +0800

    首页

---
+++ lunwen/src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <div class="w">
+      <!-- 头部 -->
+      <div class="title">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+</style>
--- lunwen/src/main.js
+++ lunwen/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import 'font-awesome/scss/font-awesome.scss'
+import VueScroll from 'vuescroll'
+import 'vuescroll/dist/vuescroll.css'
+import './assets/css/base.css'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
